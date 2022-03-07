cb60a4dd183f6cb4a27786c8a79e097c05e83f63
IOANNIS CHOURPOULIADIS <jonhiedarn@gmail.com>
2022-03-07 11:58:27 +0200

    added first 2 rows of the a rampaging power table

--- app/src/main/java/com/example/myapplication/characters/NarutoUzumaki.java
+++ app/src/main/java/com/example/myapplication/characters/NarutoUzumaki.java
@@ -2,9 +2,12 @@ package com.example.myapplication.characters;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.cards_tools.naruto_uzumaki.ultimate_jutsus_5_star.ARampagingPower;
 
 public class NarutoUzumaki extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class NarutoUzumaki extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_naruto_uzumaki);
+
+        ImageView naruto_uzumaki_ult1_artwork = findViewById(R.id.naruto_uzumaki_ult1_artwork_id);
+        naruto_uzumaki_ult1_artwork.setOnClickListener(v -> openARampagingPower());
+    }
+
+    private void openARampagingPower() {
+        Intent intent = new Intent(this, ARampagingPower.class);
+        startActivity(intent);
     }
 }
