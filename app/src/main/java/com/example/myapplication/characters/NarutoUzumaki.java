21369d84aa895de95c80cb0f29c47781dd7ebc30
IOANNIS CHOURPOULIADIS <jonhiedarn@gmail.com>
2022-03-07 10:37:18 +0200

    added naruto uzumaki activity and ultimates jutsus 5 star

---
+++ app/src/main/java/com/example/myapplication/characters/NarutoUzumaki.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.characters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class NarutoUzumaki extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_naruto_uzumaki);
+    }
+}
