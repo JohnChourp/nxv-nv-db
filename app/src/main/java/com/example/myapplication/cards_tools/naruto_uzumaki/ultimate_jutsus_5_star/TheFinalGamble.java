c01038ef449c9bd1907db341c4d8076a27ead7b5
IOANNIS CHOURPOULIADIS <jonhiedarn@gmail.com>
2022-03-07 21:45:32 +0200

    added 2 more ultimate jutsus

---
+++ app/src/main/java/com/example/myapplication/cards_tools/naruto_uzumaki/ultimate_jutsus_5_star/TheFinalGamble.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.cards_tools.naruto_uzumaki.ultimate_jutsus_5_star;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class TheFinalGamble extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_final_gamble);
+    }
+}
--- app/src/main/java/com/example/myapplication/characters/NarutoUzumaki.java
+++ app/src/main/java/com/example/myapplication/characters/NarutoUzumaki.java
@@ -8,6 +8,8 @@ import android.widget.ImageView;
 
 import com.example.myapplication.R;
 import com.example.myapplication.cards_tools.naruto_uzumaki.ultimate_jutsus_5_star.ARampagingPower;
+import com.example.myapplication.cards_tools.naruto_uzumaki.ultimate_jutsus_5_star.NarutovsPain;
+import com.example.myapplication.cards_tools.naruto_uzumaki.ultimate_jutsus_5_star.TheFinalGamble;
 
 public class NarutoUzumaki extends AppCompatActivity {
 
@@ -17,7 +19,23 @@ public class NarutoUzumaki extends AppCompatActivity {
         setContentView(R.layout.activity_naruto_uzumaki);
 
         ImageView naruto_uzumaki_ult1_artwork = findViewById(R.id.naruto_uzumaki_ult1_artwork_id);
-        naruto_uzumaki_ult1_artwork.setOnClickListener(v -> openARampagingPower());
+        naruto_uzumaki_ult1_artwork.setOnClickListener(v -> openNarutovsPain());
+
+        ImageView naruto_uzumaki_ult2_artwork = findViewById(R.id.naruto_uzumaki_ult2_artwork_id);
+        naruto_uzumaki_ult2_artwork.setOnClickListener(v -> openTheFinalGamble());
+
+        ImageView naruto_uzumaki_ult3_artwork = findViewById(R.id.naruto_uzumaki_ult3_artwork_id);
+        naruto_uzumaki_ult3_artwork.setOnClickListener(v -> openARampagingPower());
+    }
+
+    private void openNarutovsPain() {
+        Intent intent = new Intent(this, NarutovsPain.class);
+        startActivity(intent);
+    }
+
+    private void openTheFinalGamble() {
+        Intent intent = new Intent(this, TheFinalGamble.class);
+        startActivity(intent);
     }
 
     private void openARampagingPower() {
